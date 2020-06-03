1099d0dd11d642c1296cb6ce6c8177c06a240465
Frankie379350068 <379350068@qq.com>
2020-06-04 00:02:34 +0800

    收货地址之三级联动

--- meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/apps/areas/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 查询省份地址: http://www.meiduo.site:8000/areas/
     path('areas/', views.ProvinceAreasView.as_view()),
-
-
+    # 查询父级下属地址: http://www.meiduo.site:8000/areas/(?P<pk>[1-9]\d+)/
+    path('areas/<int:parentid>/', views.SubsAreasView.as_view()),
 ]
--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 from django.views import View
+
 from apps.areas.models import Area
 from django import http
 
 # Create your views here.
 
+
+class SubsAreasView(View):
+    # 查询父级下属地址: http://www.meiduo.site:8000/areas/(?P<pk>[1-9]\d+)/
+    def get(self, request, parentid):
+        # 通过id找到满足条件的地区，如id是140000，则对应找到的是山西省
+        parent_area = Area.objects.get(id=parentid)
+        # 一查多，父级对应多个子级， 如省下有多个市，市下有多个区,  模型类对象.模型类类名小写_set.all()
+        # sub_model_list = parent_area.area_set.all(), 因为Area模型类中有relate_name=subs，所以该方法不能用
+        sub_model_list = parent_area.subs.all()
+        subs = []
+        for sub_model in sub_model_list:
+            sub_dict = {
+                'id': sub_model.id,
+                'name': sub_model.name,
+            }
+            subs.append(sub_dict)
+        sub_data = {
+            'id': parent_area.id,
+            'name': parent_area.name,
+            'subs': subs,
+        }
+        return http.JsonResponse({'code': 0, 'errmsg': 'ok', 'sub_data': sub_data})
+
+
+
 class ProvinceAreasView(View):
-    # 用户添加邮箱地址: http://www.meiduo.site:8080/areas/
+    # 查询省份地址: http://www.meiduo.site:8000/areas/
 
     def get(self, request):
         # 查询省份数据
